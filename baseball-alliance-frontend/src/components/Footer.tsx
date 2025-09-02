67aa5111346d32bad00f81649816c011fa457772
Gunnar Smith <gunnarsmith3@gmail.com>
2025-09-02 09:39:39 -0500

    update links

--- baseball-alliance-frontend/src/components/Footer.tsx
+++ baseball-alliance-frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/baseballalliance.png";
-import { FaInstagram, FaYoutube, FaTiktok } from "react-icons/fa";
+import { FaInstagram, FaTiktok } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
